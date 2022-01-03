87c4c00f00a3cc850f7e36e3c4b6dab03db81e03
Siddhant Khare <Siddhantkhare2694@gmail.com>
2022-01-03 16:04:35 +0530

    feat: Peerlist Integration Added

--- components/InfoHeader.js
+++ components/InfoHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './InfoHeader.module.css';
 // import TwitterIcon from '/twitter.svg';
 // import GitHubIcon from '/GitHub.svg';
-export default function InfoHeader({ name = '', github = '', twitter = '' }) {
+export default function InfoHeader({ name = '', github = '', twitter = '', peerlist= '' }) {
   return (
     <div className={styles['info-container']}>
       <h1 className={styles['title']}>{name}</h1>
@@ -21,6 +21,15 @@ export default function InfoHeader({ name = '', github = '', twitter = '' }) {
             <img
               src="/github.svg"
               alt="github-icon"
+              className={styles['social-icon']}
+            />
+          </a>
+        )}
+        {peerlist && (
+          <a href={peerlist}>
+            <img
+              src="/peerlist.svg"
+              alt="peerlist-icon"
               className={styles['social-icon']}
             />
           </a>
